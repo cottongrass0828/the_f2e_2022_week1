c1d4aa80ae43f4fcaca396aa4c590fc74506a636
cottongrass0828 <inoriro.ame@gmail.com>
2022-11-21 00:33:39 +0800

    block1 edit

---
+++ js/381.23ebfb9a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkthe_f2e_2022_week1"]=self["webpackChunkthe_f2e_2022_week1"]||[]).push([[381],{7381:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var u=t(3396);const a={class:"about"},r=(0,u._)("h1",null,"This is an about page",-1),s=[r];function c(e,n){return(0,u.wg)(),(0,u.iD)("div",a,s)}var f=t(89);const i={},h=(0,f.Z)(i,[["render",c]]);var o=h}}]);
+//# sourceMappingURL=381.23ebfb9a.js.map
